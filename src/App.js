8c96ab50d781083be68056ab0ccab1a556316eb1
Prashant varma <prashantvarma5083@gmail.com>
2023-05-27 23:52:10 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import "./styles/Home.css";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <main className="main">
+      <div className="connect">
+          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center'}} />
+        </div>
+      </main>
+    </div>
+  );
+}
